9a48f4691576813b4beb844de6b68f6d52026b35
Saul Hardman <hello@iamsaul.co.uk>
2012-11-10 01:05:09 +0000

    Made some changes to the dry visualisation

--- website/js/source/main.js
+++ website/js/source/main.js
@@ -1,12 +1,36 @@
-require(['plugins/log', 'jquery', 'socket.io', 'plugins/paper'], function (log, $) {
+require(['plugins/log', 'jquery', 'socket.io', 'plugins/jquery.easing', 'plugins/jquery.color'], function (log, $) {
 	
 	var main = {
 
-		heat: 0,
+		heat: {
 
-		light: 0,
+			value: 0,
 
-		showLabels: true,
+			max: 30,
+
+			$value: $('#heat .value'),
+
+			$element: $('#heat .column')
+
+		},
+
+		light: {
+
+			value: 0,
+
+			max: 100,
+
+			$value: $('#sound .value'),
+
+			$element: $('#sound .column')
+
+		},
+
+		$canvas: $('#canvas'),
+
+		width: $(window).width(),
+
+		height: $(window).height(),
 
 		init: function () {
 
@@ -14,13 +38,9 @@ require(['plugins/log', 'jquery', 'socket.io', 'plugins/paper'], function (log, 
 
 			self.socketConnect();
 
-			if ($(window).width() <= 480) {
+			//self.randomise();
 
-				self.showLabels = false;
-
-			}
-
-			// console.log('Main initiated: ', self);
+			console.log('Main initiated: ', self);
 
 		},
 
@@ -48,6 +68,8 @@ require(['plugins/log', 'jquery', 'socket.io', 'plugins/paper'], function (log, 
 
 				self.heat = parseInt(data.reading);
 
+				self.animate(self.heat);
+
 				// console.log('Heat data:', self.heat);
 
 			});
@@ -56,121 +78,77 @@ require(['plugins/log', 'jquery', 'socket.io', 'plugins/paper'], function (log, 
 
 				self.light = parseInt(data.reading / 10);
 
+				self.animate(self.light);
+
 				// console.log('Light data:', self.light);
 
 			});
 
+		},
+
+		animate: function (reading) {
+
+			var self = this;
+
+			if (reading.$value.text() !== reading.value) {
+
+				$({value: reading.$value.text()}).animate({
+
+					value: reading.value
+
+				}, {
+
+					duration: 500,
+
+					step: function () {
+
+						reading.$value.text(Math.round(this.value));
+
+					}
+
+				});
+
+				reading.$element.stop().animate({
+
+					height: ((reading.value / reading.max) * 100) + '%'
+
+				}, 500, 'easeInOutQuad');
+
+			}
+
+		},
+
+		randomise: function () {
+
+			var self = this;
+
+			setTimeout(function () {
+
+				self.heat.value = getRandomInt(10, self.heat.max);
+
+				self.light.value = getRandomInt(30, self.light.max);
+
+				self.animate(self.heat);
+
+				self.animate(self.light);
+
+				self.randomise();
+
+			}, 1000);
+
+			function getRandomInt (min, max) {
+
+				return Math.floor(Math.random() * (max - min + 1)) + min;
+
+			}
+
 		}
 
 	};
-	paper.install(window);
+
 	$(document).ready(function () {
 
 		main.init();
-
-		var canvas = $('#dryCanvas')[0];
-
-		paper.setup(canvas);
-		
-		var stageW = view.size.width;
-    	var stageH = view.size.height;
-
-    var lightBg = new Path.Rectangle(0,0, stageW/2, stageH);
-    lightBg.fillColor = '#3eb555';
-    lightBg.opacity = 0.25;
-    
-    var heatBg = new Path.Rectangle(stageW/2, 0, stageW/2, stageH);
-    heatBg.fillColor = '#d05f0a';
-    heatBg.opacity = 0.25;
-
-    var lightRect = new Path.Rectangle(0,stageH, stageW/2, stageH);
-    lightRect.fillColor = '#3eb555';
-    
-    var heatRect = new Path.Rectangle(stageW/2, stageH, stageW/2, stageH);
-    heatRect.fillColor = '#d05f0a';
-
-    var heatText = new PointText(new Point(stageW - 25, 150));
-    heatText.justification = 'right';
-    heatText.fillColor = 'white';
-    heatText.fontSize = 120;       
-    heatText.content = main.heat;   
-
-    var lightText = new PointText(new Point(stageW/2 - 25, 150));
-    lightText.justification = 'right';
-    lightText.fillColor = 'white';
-    lightText.fontSize = 120;
-    lightText.content = main.light; 
-
-    if (main.showLabels) {
-
-	    var lightLabel = new Path.Rectangle(0, stageH-150, stageW/2, 150);
-	    lightLabel.fillColor = 'black';
-	    lightLabel.opacity = 0.6;
-	    
-	    var heatLabel = new Path.Rectangle(stageW/2, stageH-150, stageW/2, 150);
-	    heatLabel.fillColor = 'black';
-	    heatLabel.opacity = 0.6; 
-
-	    var heatLabelText = new PointText(new Point((stageW/4)*3, stageH-45));
-	    heatLabelText.justification = 'center';
-	    heatLabelText.fillColor = 'white';
-	    heatLabelText.fontSize = 80;        
-	    heatLabelText.content = "Heat";   
-
-	    var lightLabelText = new PointText(new Point(stageW/4, stageH-45));
-	    lightLabelText.justification = 'center';
-	    lightLabelText.fillColor = 'white';
-	    lightLabelText.fontSize = 80;
-	    lightLabelText.content = "Light";
-
-	}
-
-    function scrollNum(value, current){ 
-        var diff = value - current;
-        if(current != value){
-            if(diff > 50){
-                current = parseInt(current) + 50
-            } else if(diff > 10){
-                current = parseInt(current) + 10
-            } else if(diff > 0){
-                current = parseInt(current) + 1
-            } else if(diff < 0){
-                current = parseInt(current) - 1
-            } else if(diff < -10){
-                current = parseInt(current) - 10
-            } else if(diff < -50){
-                current = parseInt(current) - 50
-            }
-        }
-        return current;
-    }
-    function updateBars(theBar, theVar){
-        var range;
-        if (theBar == heatRect){
-            range = 40;
-        } else if (theBar == lightRect) {
-            range = 100;
-        }
-        
-       var theHeight = parseInt((stageH * ( 1-(theVar / range))));
-
-        for (var i = 1; i < 3; i++){
-             if(theBar.segments[i].point.y != theHeight){
-                var vector = (theHeight - theBar.segments[i].point.y) ;
-                theBar.segments[i].point.y += parseInt(vector/5);
-            }
-        }
-    }
-
-    view.onFrame = function(event) {
-        heatText.content = scrollNum(main.heat, heatText.content);
-        lightText.content = scrollNum(main.light, lightText.content);
-        updateBars(heatRect, main.heat);
-        updateBars(lightRect, main.light);
-      
-         lightText.position.y = lightRect.segments[1].point.y + 140;
-         heatText.position.y = heatRect.segments[1].point.y + 140;
-    }
 	
 	});
 
